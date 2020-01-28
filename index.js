354960f892bee96b7d9526c23b97d2ce821bbce7
Anthony <anthony.kizer88@gmail.com>
2020-01-27 22:15:21 -0800

    completed all problems

--- index.js
+++ index.js
@@ -102,7 +102,16 @@ Car.prototype.drive = function(distance) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {}
+function Baby(name, age, favoriteToy) {
+  this.favoriteToy = favoriteToy;
+  Person.call(this, name, age);
+}
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
